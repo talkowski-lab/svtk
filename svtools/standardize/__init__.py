2077e718cf4a8c8ec874801183b607b1db92d20f
Matt Stone <mstone@chgr.mgh.harvard.edu>
2017-04-21 09:52:50 -0400

    init update

--- svtools/standardize/__init__.py
+++ svtools/standardize/__init__.py
@@ -1,1 +1,5 @@
 from .standardize import VCFStandardizer
+from .std_delly import DellyStandardizer
+from .std_wham import WhamStandardizer
+from .std_lumpy import LumpyStandardizer
+from .std_manta import MantaStandardizer
