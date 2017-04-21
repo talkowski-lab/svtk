e64475a47ecade60c868ada84b87494236a5c823
Matt Stone <mstone@chgr.mgh.harvard.edu>
2017-04-21 09:05:53 -0400

    switch wham to VCFStandardizer

--- svtools/standardize/std_wham.py
+++ svtools/standardize/std_wham.py
@@ -8,36 +8,41 @@ Distributed under terms of the MIT license.
 """
 
 
-def standardize_wham(raw_rec, std_rec):
-    """
-    Standardize Wham record.
-    """
+from .standardize import VCFStandardizer
 
-    # colons in the ID break PyVCF parsing
-    std_rec.id = '_'.join(std_rec.id.split(':'))
 
-    # commas conflict with VCF format field with num='.'
-    std_rec.id = ';'.join(std_rec.id.split(','))
+@VCFStandardizer.register('wham')
+class WhamStandardizer(VCFStandardizer):
+    def standardize_info(self, std_rec, raw_rec):
+        """
+        Standardize Wham record.
+        """
 
-    # SV types already standard
-    std_rec.info['SVTYPE'] = raw_rec.info['SVTYPE']
+        # colons in the ID break PyVCF parsing
+        std_rec.id = '_'.join(std_rec.id.split(':'))
 
-    # No CTX events
-    std_rec.info['CHR2'] = std_rec.chrom
-    std_rec.info['END'] = raw_rec.info['END']
+        # commas conflict with VCF format field with num='.'
+        std_rec.id = ';'.join(std_rec.id.split(','))
 
-    # Strand not provided for inv/tloc
-    if std_rec.info['SVTYPE'] == 'DEL':
-        strands = '+-'
-    elif std_rec.info['SVTYPE'] == 'DUP':
-        strands = '-+'
-    else:
-        strands = '.'
-    std_rec.info['STRANDS'] = strands
+        # SV types already standard
+        std_rec.info['SVTYPE'] = raw_rec.info['SVTYPE']
 
-    # SVLEN is a list and can be negative
-    std_rec.info['SVLEN'] = abs(raw_rec.info['SVLEN'][0])
+        # No CTX events
+        std_rec.info['CHR2'] = std_rec.chrom
+        std_rec.info['END'] = raw_rec.info['END']
 
-    std_rec.info['SOURCE'] = 'wham'
+        # Strand not provided for inv/tloc
+        if std_rec.info['SVTYPE'] == 'DEL':
+            strands = '+-'
+        elif std_rec.info['SVTYPE'] == 'DUP':
+            strands = '-+'
+        else:
+            strands = '.'
+        std_rec.info['STRANDS'] = strands
 
-    return std_rec
+        # SVLEN is a list and can be negative
+        std_rec.info['SVLEN'] = abs(raw_rec.info['SVLEN'][0])
+
+        std_rec.info['SOURCE'] = 'wham'
+
+        return std_rec
