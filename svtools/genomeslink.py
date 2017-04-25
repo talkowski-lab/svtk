e90096f1d69653d0f4379de38660fc8053122b8d
Matt Stone <mstone@chgr.mgh.harvard.edu>
2017-04-25 16:00:18 -0400

    moved is_smaller_chrom to utils

--- svtools/genomeslink.py
+++ svtools/genomeslink.py
@@ -17,30 +17,7 @@ import numpy as np
 from scipy import sparse
 from scipy.sparse import csgraph
 
-
-def is_smaller_chrom(chrA, chrB):
-    """
-    Test if chrA is naturally less than chrB
-
-    Returns True if chrA == chrB so comparison will default to position
-    """
-
-    if chrA.startswith('chr'):
-        chrA = chrA[3:]
-    if chrB.startswith('chr'):
-        chrB = chrB[3:]
-
-    # Numeric comparison, if possible
-    if chrA.isdigit() and chrB.isdigit():
-        return int(chrA) <= int(chrB)
-
-    # String comparison for X/Y
-    elif not chrA.isdigit() and not chrB.isdigit():
-        return chrA <= chrB
-
-    # Numeric is always less than X/Y
-    else:
-        return chrA.isdigit()
+from .utils import is_smaller_chrom
 
 
 class GSNode(object):
