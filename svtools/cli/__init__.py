8c64e55f81771497e7c44c2868980e671c8a7898
Matt Stone <mstone@chgr.mgh.harvard.edu>
2017-05-04 16:56:40 -0400

    rename standardize

--- svtools/cli/__init__.py
+++ svtools/cli/__init__.py
@@ -1,4 +1,4 @@
 from .vcfcluster import main as vcfcluster
 from .bedcluster import main as bedcluster
-from .standardize_vcf import main as standardize_vcf
+from .standardize_vcf import main as standardize
 from .bincov import main as bincov
