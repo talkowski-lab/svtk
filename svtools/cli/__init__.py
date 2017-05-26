cfe7b8418b17ef002c122ab90ba74143fac6023d
Matt Stone <mstone@chgr.mgh.harvard.edu>
2017-05-26 09:24:20 -0400

    Add tool for counting per-sample svtypes in a VCF

--- svtools/cli/__init__.py
+++ svtools/cli/__init__.py
@@ -1,4 +1,5 @@
 from .vcfcluster import main as vcfcluster
 from .bedcluster import main as bedcluster
 from .standardize_vcf import main as standardize
+from .count_svtypes import main as count_svtypes
 from .bincov import main as bincov
---
+++ svtools/cli/count_svtypes.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2017 Matthew Stone <mstone5@mgh.harvard.edu>
+# Distributed under terms of the MIT license.
+
+"""
+Count the instances of each SVTYPE observed in each sample in a VCF.
+
+A record is only counted towards a sample's total if the sample received a
+non-reference (0/0) or a non-null (./.) call. Records without an annotated
+SVTYPE in the INFO field are counted as "NO_SVTYPE".
+
+Counts are reported in a three column table (sample, svtype, count).
+"""
+
+import argparse
+import sys
+from collections import defaultdict
+import pandas as pd
+from pysam import VariantFile
+from svtools.utils import NULL_GT
+
+
+def count_svtypes(vcf):
+    """
+    Count instances of each SVTYPE in each sample in a VCF.
+
+    Parameters
+    ----------
+    vcf : pysam.VariantFile
+
+    Returns
+    -------
+    counts : pd.DataFrame
+        Columns: sample, svtype, count
+    """
+
+    samples = list(vcf.header.samples)
+
+    # Initialize counts per sample - each dict is keyed on svtype
+    count_dict = {}
+    for sample in samples:
+        count_dict[sample] = defaultdict(int)
+
+    for record in vcf:
+        for sample in samples:
+            # Only count called variants
+            gt = record.samples[sample]['GT']
+            if gt in NULL_GT:
+                continue
+
+            # Count the SVTYPE if it's present, otherwise increment NO_SVTYPE
+            if 'SVTYPE' in record.info.keys():
+                count_dict[sample][record.info['SVTYPE']] += 1
+            else:
+                count_dict[sample]['NO_SVTYPE'] += 1
+
+    # Convert to dataframe, adding zeros to samples with no instances of a
+    # given svtype
+    counts = pd.DataFrame.from_dict(count_dict, orient='index')\
+                         .fillna(0).astype(int)\
+                         .reset_index().rename(columns={'index': 'sample'})
+
+    # Tidy data from "column-per-svtype" format
+    counts = pd.melt(counts, id_vars=['sample'],
+                     var_name='svtype', value_name='count')
+
+    return counts
+
+
+def main(argv):
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('vcf')
+    parser.add_argument('--no-header', action='store_true', default=False,
+                        help="Don't include header in output")
+    parser.add_argument('fout', type=argparse.FileType('w'), nargs='?',
+                        default=sys.stdout, help='Output file [stdout]')
+    args = parser.parse_args(argv)
+
+    vcf = VariantFile(args.vcf)
+    counts = count_svtypes(vcf)
+
+    header = not args.no_header
+    counts.to_csv(args.fout, sep='\t', index=False, header=header)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
