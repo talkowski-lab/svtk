1d403511035ae5f7baff14d4d0fca6c9858d88ed
Matt Stone <mstone@chgr.mgh.harvard.edu>
2017-06-27 16:42:10 -0400

    MELT standardization

--- svtools/standardize/__init__.py
+++ svtools/standardize/__init__.py
@@ -3,3 +3,4 @@ from .std_delly import DellyStandardizer
 from .std_wham import WhamStandardizer
 from .std_lumpy import LumpyStandardizer
 from .std_manta import MantaStandardizer
+from .std_melt import MeltStandardizer
