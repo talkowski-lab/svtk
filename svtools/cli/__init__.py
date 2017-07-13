cd1676d5f32d6d88e8f9f7245ad51b65c1d78c8b
Matt Stone <mstone@chgr.mgh.harvard.edu>
2017-07-13 14:10:46 -0400

    rdtest2vcf

--- svtools/cli/__init__.py
+++ svtools/cli/__init__.py
@@ -4,3 +4,5 @@ from .standardize_vcf import main as standardize
 from .count_svtypes import main as count_svtypes
 from .bincov import main as bincov
 from .sr_test import main as sr_test
+from .pe_test import main as pe_test
+from .rdtest2vcf import main as rdtest2vcf
