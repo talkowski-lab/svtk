10b1be49f3008e34c13dced4d178fd9020edc744
Matt Stone <mstone@chgr.mgh.harvard.edu>
2017-07-25 14:07:43 -0400

    write unresolved breakpoints to bed

--- svtools/cli/link_cx.py
+++ svtools/cli/link_cx.py
@@ -22,7 +22,9 @@ def main(argv):
     parser.add_argument('vcf', help='Breakpoint VCFs.')
     parser.add_argument('bed', type=argparse.FileType('w'),
                         help='Resolved cx variants.')
-    parser.add_argument('stats')
+    parser.add_argument('unresolved', type=argparse.FileType('w'),
+                        help='Unresolved cx breakpoints.')
+    #  parser.add_argument('stats')
     parser.add_argument('-p', '--prefix', default='CPX_',
                         help='Variant prefix [CPX_]')
 
@@ -31,24 +33,29 @@ def main(argv):
         sys.exit(1)
     args = parser.parse_args(argv)
 
-    cx_counts = defaultdict(int)
+    #  cx_counts = defaultdict(int)
 
+    resolved_classes = ('INV delINV INVdel delINVdel dupINV INVdup dupINVdup '
+                        'delINVdup dupINVdel DUP5/INS3 DUP3/INS5').split()
     clusters = link_cx(args.vcf)
     for i, cluster in enumerate(clusters):
         cx_type, entry = resolve_cx(cluster)
-        cx_counts[cx_type] += 1
-        if entry is None:
-            continue
+        #  cx_counts[cx_type] += 1
+        #  if entry is None:
+            #  continue
         entry = entry.format(name=args.prefix + str(i + 1))
-        args.bed.write(entry)
 
-    #  cx_types = ('SIMPLE delINV INVdel delINVdel dupINV INVdup dupINVdup '
-                #  'delINVdup dupINVdel DUP5/INS3 DUP3/INS5 UNK '
-                #  'SINGLE_ENDER COMPLEX_3plus MATCHED_STRANDS MIXED_INV_TLOC '
-                #  'INTERCHROMOSOMAL ERROR_CNV_ONLY '
-                #  'CNV_2_FAIL CNV_1_unclassified').split()
-    cx_counts = pd.Series(cx_counts)#.reindex(cx_types).fillna(0).astype(int)
-    cx_counts.to_csv(args.stats, sep='\t')
+        if cx_type in resolved_classes:
+            args.bed.write(entry)
+        else:
+            args.unresolved.write(entry)
+
+    #  'SINGLE_ENDER COMPLEX_3plus MATCHED_STRANDS MIXED_INV_TLOC '
+    #  'INTERCHROMOSOMAL ERROR_CNV_ONLY '
+    #  'CNV_2_FAIL CNV_1_unclassified').split()
+
+    #  cx_counts = pd.Series(cx_counts)
+    #  cx_counts.to_csv(args.stats, sep='\t')
 
 
 if __name__ == '__main__':
