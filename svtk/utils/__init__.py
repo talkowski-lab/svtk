8a2cddbcb3687518ea3fc61819254d151bacbd7a
Matt Stone <mstone@chgr.mgh.harvard.edu>
2018-05-30 16:00:53 -0400

    add rdtest to init

--- svtk/utils/__init__.py
+++ svtk/utils/__init__.py
@@ -4,3 +4,4 @@ from .s3bam import load_s3bam
 from .helpers import is_excluded, is_soft_clipped, reciprocal_overlap, overlap_frac
 from .multi_tabixfile import MultiTabixFile
 from .genotype_merging import update_best_genotypes
+from .rdtest import call_rdtest
