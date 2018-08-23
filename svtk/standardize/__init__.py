23eae6defb4c6cdde106adb858f2c9714693b57a
awacs <haroldzwang@gmail.com>
2018-08-23 15:41:02 -0400

    Add standardizer for smoove (#73)
    * smoove
    
    * smoove
    
    * init
    
    * smoove modify
    
    * smoove modify 2
    
    * smoove modify 3

--- svtk/standardize/__init__.py
+++ svtk/standardize/__init__.py
@@ -4,3 +4,4 @@ from .std_wham import WhamStandardizer
 from .std_lumpy import LumpyStandardizer
 from .std_manta import MantaStandardizer
 from .std_melt import MeltStandardizer
+from .std_smoove import SmooveStandardizer
