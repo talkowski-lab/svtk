622fdf3297a65df65eaa288935d3ccb4a83f5aeb
awacs <haroldzwang@gmail.com>
2018-09-06 11:04:34 -0400

    Smoove (#78)
    * don't filter smoove
    
    * smoove bug
    
    * yolo2
    
    * further smoove fix
    
    * rawrec
    
    * duplicate definitions in smoove and lumpy

--- svtk/standardize/std_smoove.py
+++ svtk/standardize/std_smoove.py
@@ -88,7 +88,7 @@ class SmooveStandardizer(VCFStandardizer):
 
         # Any sample in TAGS field is considered to be called
         for sample in raw_rec.samples:
-            if std_rec.samples[sample]['GT'] == (0, 1):
+            if raw_rec.samples[sample]['GT'] == (0, 1):
                 std_rec.samples[sample]['GT'] = (0, 1)
                 std_rec.samples[sample][source] = 1
             else:
@@ -120,22 +120,4 @@ class SmooveStandardizer(VCFStandardizer):
                 std_rec.info['SVTYPE'] = 'INV'
                 std_rec.alts = ('<INV>', )
         std_rec.stop = stop
-        return std_rec
-
-    def standardize_format(self, std_rec, raw_rec):
-        """
-        Parse called samples from TAGS field
-        """
-
-        source = std_rec.info['ALGORITHMS'][0]
-
-        # Any sample in TAGS field is considered to be called
-        for sample in raw_rec.samples:
-            if std_rec.samples[sample]['GT'] == (0, 1):
-                std_rec.samples[sample]['GT'] = (0, 1)
-                std_rec.samples[sample][source] = 1
-            else:
-                std_rec.samples[sample]['GT'] = (0, 0)
-                std_rec.samples[sample][source] = 0
-
         return std_rec
